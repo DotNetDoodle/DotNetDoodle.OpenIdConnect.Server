4e89d6fb5c6e6abd4d8754aced908e596ec08375
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2014-03-12 15:11:55 +0200

    initial load

---
+++ src/DotNetDoodle.OpenIdConnect.Server/Constants.cs
@@ -0,0 +1,29 @@
+﻿
+namespace DotNetDoodle.OpenIdConnect.Server
+{
+    public static class Constants
+    {
+        /// <summary>
+        /// Standard Claims as defined at http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+        /// </summary>
+        public static class StandardClaims
+        {
+            /// <summary>
+            /// Identifier for the End-User at the Issuer.
+            /// </summary>
+            public const string Subject = "sub";
+
+            /// <summary>
+            /// End-User's full name in displayable form including all name parts, possibly including 
+            /// titles and suffixes, ordered according to the End-User's locale and preferences.
+            /// </summary>
+            public const string Name = "name";
+
+            /// <summary>
+            /// Given name(s) or first name(s) of the End-User. Note that in some cultures, people can have multiple 
+            /// given names; all can be present, with the names being separated by space characters.
+            /// </summary>
+            public const string GivenName = "given_name";
+        }
+    }
+}
---
+++ src/DotNetDoodle.OpenIdConnect.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetDoodle.OpenIdConnect.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetDoodle.OpenIdConnect.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96f23f1d-b155-4466-843a-7e62ebaf1300")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
